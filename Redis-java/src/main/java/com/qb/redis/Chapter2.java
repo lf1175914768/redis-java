ead42861dad72e3845ebe8f6d7580ee5b7c9eccf
lf1175914768 <1175914768@qq.com>
2018-09-06 23:39:30 +0800

    初始化项目，添加几个Util类

---
+++ Redis-java/src/main/java/com/qb/redis/Chapter2.java
@@ -0,0 +1,10 @@
+package com.qb.redis;
+
+/**
+ * 在这里对第二章节的内容进行分析
+ * @author Liufeng
+ * Created on 2018年9月6日 下午10:53:45
+ */
+public class Chapter2 {
+
+}
---
+++ Redis-java/src/main/java/com/qb/utils/JedisService.java
@@ -0,0 +1,29 @@
+package com.qb.utils;
+
+/**
+ * @author Liufeng
+ * Created on 2018年9月6日 下午11:10:56
+ */
+public interface JedisService {
+	
+	/**
+	 * The default password.
+	 */
+	String DEFAULT_PASSWORD = "xyyyhtl12";
+	
+	/**
+	 * The default port.
+	 */
+	int DEFAULT_PORT = 6379;
+	
+	/**
+	 * The default host.
+	 */
+	String DEFAULT_HOST = "localhost";
+	
+	/**
+	 * The default timeout.
+	 */
+	int DEFAULT_TIMEOUT = 10000;
+
+}
